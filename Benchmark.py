0b73eadfbe6aa89b955ebe0c563396041189906c
MarcChedefaux <chedefaux.marc@gmail.com>
2024-06-16 22:35:22 +0200

    15mins run on benchmark

--- Benchmark.py
+++ Benchmark.py
@@ -12,17 +12,19 @@ if __name__ == "__main__":
     solver = BasicBacktracker()
     solver.Init()
 
-    df_sample = df.sample(frac=0.000001)
-
     times = []
 
-    for index, rows in tqdm(df_sample.iterrows(), total=df_sample.shape[0]):
+    benchmark_start = time.perf_counter()
+    for index, rows in tqdm(df.iterrows(), total=df.shape[0]):
         grid = Grid(rows.puzzle)
         start_time = time.perf_counter()
         result = solver.Compute(grid)
         stop_time = time.perf_counter()
 
         times.append(stop_time - start_time)
+
+        if time.perf_counter() - benchmark_start >= 15 * 60:
+            break
 
         assert result.to_string() == rows.solution
 
